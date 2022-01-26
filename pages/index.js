57e3b86d4385c6d8a40b1c63fa02a9e62cca61a2
Pranav <pranav.techiegeek@gmail.com>
2022-01-26 10:20:28 +0530

    changed class to className

--- pages/index.js
+++ pages/index.js
@@ -12,35 +12,35 @@ export default function Home() {
       </Head>
 
       <main>
-        <section class="text-gray-600 body-font">
-          <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <section className="text-gray-600 body-font">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
               <Image
-                class="object-cover object-center rounded"
+                className="object-cover object-center rounded"
                 alt="hero"
                 src="/stock-image.jpg"
                 height="600"
                 width="720"
               />
             </div>
-            <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                 Time Table Planner
               </h1>
-              <p class="mb-8 leading-relaxed">
+              <p className="mb-8 leading-relaxed">
                 A single place to plan / draft / manage your classes. Create
                 your timetable and have a look at the calendar and see all your
                 scheduled classes in one place. You can also unenroll in the
                 courses you don't like.
               </p>
-              <div class="flex justify-center">
+              <div className="flex justify-center">
                 <Link href="/register">
-                  <a class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
+                  <a className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
                     Register
                   </a>
                 </Link>
                 <Link href="/login">
-                  <a class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  <a className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
                     Login
                   </a>
                 </Link>
